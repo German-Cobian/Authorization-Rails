422d013f3bdfc44ace01f591c99621440d4e1c51
German Renato Cobian <germancobian@hotmail.com>
2023-12-21 11:24:33 -0800

    Include additional variables and JSON responses in users_controller show method + change ports for Rails app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,17 +13,21 @@ class UsersController < ApplicationController
     @room = Room.new
     @rooms = Room.public_rooms
     @private_rooms = Room.private_rooms
-    
+    @room_name = get_name(@user, current_user)
+    #@single_room = Room.where(name: @room_name).first || Room.create_private_room([@user, current_user], @room_name)  
+ 
     @message = Message.new
-
+    #@messages = @single_room.messages.order(created_at: :asc)
+    #@messages_private = @private_rooms.messages.order(created_at: :asc)
 
     render json: {
       user: UserSerializer.new(@user).serializable_hash[:data][:attributes],
       users: @users.as_json(only: [:id, :email]),
       rooms: @rooms.as_json(include: [:messages]), 
+      single_room: @single_room.as_json(include: [:messages]),
+      room_name: @room_name.as_json,
       private_rooms: @private_rooms.as_json(include: [:messages]),
-	   
-	   
+      #messages_private: @message_private.as_json,
     }, status: :ok
   end
 
@@ -69,6 +73,9 @@ class UsersController < ApplicationController
     params.require(:user).permit(:email, :is_enabled, :role, :password)
   end
 
-
+  def get_name(user1, user2)
+    user = [user1, user2].sort
+    "private_#{user[0].id}_#{user[1].id}"
+  end
 
 end
